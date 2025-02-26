ccf4cff12581d75a07c9ad848b9253d077635620
Lucas Aves <100840952+Lucas-Alves0@users.noreply.github.com>
2025-02-26 14:55:38 -0300

    Create asteroid-destroy archive

---
+++ Asteriod_Destroy.py
@@ -0,0 +1,67 @@
+import pygame
+from pygame import *
+from MyFirstClasses import *
+from random import *
+
+
+pygame.init()
+tela = display.set_mode([840, 480])
+display.set_caption('GameProject')
+
+# Sprite(personagem)
+objectGroup = sprite.Group()
+asteroidGroup = sprite.Group()
+shotGroup = sprite.Group()
+scoreGroup = sprite.Group()
+
+bg = sprite.Sprite(objectGroup)
+bg.image = image.load(r"C:\Users\lucas.paula_kovi\VSCodeProjects\MyOwnProjects\Space-Invaders-Game\game_assets\data\Cenario.png")
+bg.image = transform.scale(bg.image, [840, 480])
+bg.rect = bg.image.get_rect()
+
+player = MainPlayer(objectGroup)
+
+# Music
+mixer.music.load(r"C:\Users\lucas.paula_kovi\VSCodeProjects\MyOwnProjects\Space-Invaders-Game\game_assets\data\GameMusic.mp3")
+mixer.music.play(-1)
+mixer.music.set_volume(0.2)
+
+# Sounds
+shoot = mixer.Sound(r"C:\Users\lucas.paula_kovi\VSCodeProjects\MyOwnProjects\Space-Invaders-Game\game_assets\data\Shoot.wav")
+
+
+gameloop = True
+gameover = False
+timer = 20
+clock = time.Clock()
+
+while gameloop:
+    clock.tick(60)
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            gameloop = False
+        elif event.type == KEYDOWN:
+            if event.key == K_SPACE and not gameover:
+                shoot.play()
+                shot = Shot(objectGroup, shotGroup)
+                shot.rect.center = player.rect.center
+
+    if not gameover:
+        objectGroup.update()
+
+        timer += 1
+        if timer > 30:
+            timer = 0
+            if random() < 0.3:
+                newAsteroid = Asteroid(objectGroup, asteroidGroup)
+
+        collisionsPlayer = sprite.spritecollide(player, asteroidGroup, False, sprite.collide_mask)
+
+        if collisionsPlayer:
+            print('Game Over!')
+            gameover = True
+
+        hit = sprite.groupcollide(shotGroup, asteroidGroup, True, True, sprite.collide_mask)
+
+    objectGroup.draw(tela)
+    display.update()
---
+++ Game_Classes.py
@@ -0,0 +1,66 @@
+from pygame import *
+from random import *
+
+class MainPlayer(sprite.Sprite):
+    def __init__(self, *groups):
+        super().__init__(*groups)
+
+        self.image = image.load(r"C:\Users\lucas.paula_kovi\VSCodeProjects\MyOwnProjects\Space-Invaders-Game\game_assets\data\Player.png")
+        self.image = transform.scale(self.image, [100, 95])
+        self.rect = Rect([50, 50, 100, 70])
+
+        self.speed = 0
+        self.accelerate = 0.3
+
+    def update(self, *args):
+        keys = key.get_pressed()
+
+        if keys[K_w]:
+            self.speed -= self.accelerate
+        elif keys[K_s]:
+            self.speed += self.accelerate
+        else:
+            self.speed *= 0.92
+
+        self.rect.y += self.speed
+
+        if self.rect.top < 0:
+            self.rect.top = 0
+            self.speed = 0
+        elif self.rect.bottom > 480:
+            self.rect.bottom = 480
+            self.speed = 0
+
+
+class Asteroid(sprite.Sprite):
+    def __init__(self, *groups):
+        super().__init__(*groups)
+
+        self.image = image.load(r"C:\Users\lucas.paula_kovi\VSCodeProjects\MyOwnProjects\Space-Invaders-Game\game_assets\data\Asteroid.png")
+        self.image = transform.scale(self.image, [70, 70])
+        self.rect = Rect([50, 50, 70, 70])
+
+        self.rect.x = 840 + randint(1, 490)
+        self.rect.y = randint(1, 360)
+        self.speed = 1 + random() * 2
+
+    def update(self, *args):
+        self.rect.x -= self.speed
+
+        if self.rect.right < 0:
+            self.kill()
+
+class Shot(sprite.Sprite):
+    def __init__(self, *groups):
+        super().__init__(*groups)
+
+        self.image = image.load(r"C:\Users\lucas.paula_kovi\VSCodeProjects\MyOwnProjects\Space-Invaders-Game\game_assets\data\Shot.png")
+        self.image = transform.scale(self.image, [50, 50])
+        self.rect = Rect([50, 50, 50, 50])
+        self.speed = 4
+
+    def update(self, *args):
+        self.rect.x += self.speed
+
+        if self.rect.left > 840:
+            self.kill()
