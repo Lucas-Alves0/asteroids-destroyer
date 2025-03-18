9019f52e2da7647a6234d070c6f1222ee5744f1f
Lucas Aves <100840952+Lucas-Alves0@users.noreply.github.com>
2025-03-17 23:15:09 -0300

    Update game elements
    Nessa versão, foi :
    
    - Imagem do Player e Shot Alteradas.
    - Adicionado sistema de aumento de dificuldade, a dificuldade aumenta a cada 10 pontos de score.
    - Adicionada tela Temporária antes do Jogo iniciar.
    - Adicionado Score.
    - Eliminados Alguns arquivos desnecessários.
    - Adicionado .png do Main Menu (será adicionado na próxima versão.

--- Asteriod_Destroy.py
+++ Asteriod_Destroy.py
@@ -5,30 +5,62 @@ from random import *
 
 
 pygame.init()
-tela = display.set_mode([840, 480])
+screen_size = [840, 480]
+tela = display.set_mode(screen_size)
 display.set_caption('GameProject')
 
 # Sprite(personagem)
 objectGroup = sprite.Group()
 asteroidGroup = sprite.Group()
 shotGroup = sprite.Group()
-scoreGroup = sprite.Group()
+
+# Score Point (Player Score)
+
+player_score = 0
+level = 1
+
+pygame.font.init()
+fonte_1 = pygame.font.Font('freesansbold.ttf', 18)
+fonte_2 = pygame.font.SysFont('arialblack', 26)
+fontX = 7
+fontY = 7
+
+levelX = 750
+levelY = 7
+
+startX = 420
+startY = 240
+
+def show_score(x, y):
+    score_img = fonte_1.render(f'Score: {str(player_score)}', True, [255, 255, 255])
+    tela.blit(score_img, (x, y))
+
+def show_level(x, y):
+    level_img = fonte_1.render(f'Level: {str(level)}', True, [255, 255, 255])
+    tela.blit(level_img, (x, y))
+
+def start_msg(text, font, text_col, x, y):
+    start_img = font.render(text, True, text_col)
+    tela.blit(start_img, (x, y))
 
 bg = sprite.Sprite(objectGroup)
 bg.image = image.load(r"C:\Users\lucas.paula_kovi\VSCodeProjects\MyOwnProjects\asteroids_destroy\game_assets\data\Cenario.png")
-bg.image = transform.scale(bg.image, [840, 480])
+bg.image = transform.scale(bg.image, screen_size)
 bg.rect = bg.image.get_rect()
+
+# Define game variables
+start_game = False
+difficult = 0
+asteroid_speed = 1
 
 player = MainPlayer(objectGroup)
 
-# Music
+# Sounds
 mixer.music.load(r"C:\Users\lucas.paula_kovi\VSCodeProjects\MyOwnProjects\asteroids_destroy\game_assets\data\GameMusic.mp3")
 mixer.music.play(-1)
-mixer.music.set_volume(0.2)
+mixer.music.set_volume(0.1)
 
-# Sounds
 shoot = mixer.Sound(r"C:\Users\lucas.paula_kovi\VSCodeProjects\MyOwnProjects\asteroids_destroy\game_assets\data\Shoot.wav")
-
 
 gameloop = True
 gameover = False
@@ -36,32 +68,57 @@ timer = 20
 clock = time.Clock()
 
 while gameloop:
+
     clock.tick(60)
+
+    if start_game == False:
+        start_msg('Press SPACE to play', fonte_2, [255, 255, 255], 275, 226)
+
+    else:
+        mixer.music.set_volume(0.2)
+        
+        if not gameover:
+            objectGroup.update()
+
+            timer += 1
+            if timer > 30:
+                timer = 0
+                if random() < 0.35:
+                    newAsteroid = Asteroid(objectGroup, asteroidGroup, speed=asteroid_speed)
+
+            collisionsPlayer = sprite.spritecollide(player, asteroidGroup, False, sprite.collide_mask)
+
+            if collisionsPlayer:
+                print('Game Over!')
+                gameover = True
+
+            hit = sprite.groupcollide(shotGroup, asteroidGroup, True, True, sprite.collide_mask)
+
+            if hit:
+                player_score += 1
+                difficult += 1
+                
+            if difficult == 10:
+                asteroid_speed += 1
+                difficult = 0
+                level += 1
+                print('Dificuldade Aumentada!')
+
+        objectGroup.draw(tela)
+
+        # display score
+        show_score(fontX, fontY)
+        show_level(levelX, levelY)
+
     for event in pygame.event.get():
         if event.type == QUIT:
             gameloop = False
         elif event.type == KEYDOWN:
+            if event.key == K_ESCAPE:
+                start_game = True
             if event.key == K_SPACE and not gameover:
                 shoot.play()
                 shot = Shot(objectGroup, shotGroup)
                 shot.rect.center = player.rect.center
 
-    if not gameover:
-        objectGroup.update()
-
-        timer += 1
-        if timer > 30:
-            timer = 0
-            if random() < 0.3:
-                newAsteroid = Asteroid(objectGroup, asteroidGroup)
-
-        collisionsPlayer = sprite.spritecollide(player, asteroidGroup, False, sprite.collide_mask)
-
-        if collisionsPlayer:
-            print('Game Over!')
-            gameover = True
-
-        hit = sprite.groupcollide(shotGroup, asteroidGroup, True, True, sprite.collide_mask)
-
-    objectGroup.draw(tela)
     display.update()
--- Game_Classes.py
+++ Game_Classes.py
@@ -5,35 +5,41 @@ class MainPlayer(sprite.Sprite):
     def __init__(self, *groups):
         super().__init__(*groups)
 
-        self.image = image.load(r"C:\Users\lucas.paula_kovi\VSCodeProjects\MyOwnProjects\asteroids_destroy\game_assets\data\Player.png")
-        self.image = transform.scale(self.image, [100, 95])
-        self.rect = Rect([50, 50, 100, 70])
+        # Load the spaceship image
+        self.image = image.load(r"C:\Users\lucas.paula_kovi\VSCodeProjects\MyOwnProjects\asteroids_destroy\game_assets\data\Spaceship_move0.png")
+        self.image = transform.scale(self.image, [96, 96])
+        self.rect = Rect([96, 96, 96, 96])
 
         self.speed = 0
         self.accelerate = 0.3
 
     def update(self, *args):
+        # Get the keys pressed
         keys = key.get_pressed()
 
-        if keys[K_w]:
+        # Check if the keys are pressed
+
+        if keys[K_w]: # If the key W is pressed, the spaceship will move up
             self.speed -= self.accelerate
-        elif keys[K_s]:
+        elif keys[K_s]: # If the key S is pressed, the spaceship will move down
             self.speed += self.accelerate
         else:
-            self.speed *= 0.92
+            self.speed *= 0.92 # If no key is pressed, the spaceship will slow down
 
-        self.rect.y += self.speed
+        self.rect.y += self.speed # Move the spaceship
 
-        if self.rect.top < 0:
+        # Check if the spaceship is out of the screen
+
+        if self.rect.top < 0: # If the spaceship is out of the screen, the spaceship will be placed at the top of the screen
             self.rect.top = 0
             self.speed = 0
-        elif self.rect.bottom > 480:
+        elif self.rect.bottom > 480: # If the spaceship is out of the screen, the spaceship will be placed at the bottom of the screen
             self.rect.bottom = 480
             self.speed = 0
 
 
 class Asteroid(sprite.Sprite):
-    def __init__(self, *groups):
+    def __init__(self, *groups, speed):
         super().__init__(*groups)
 
         self.image = image.load(r"C:\Users\lucas.paula_kovi\VSCodeProjects\MyOwnProjects\asteroids_destroy\game_assets\data\Asteroid.png")
@@ -42,7 +48,7 @@ class Asteroid(sprite.Sprite):
 
         self.rect.x = 840 + randint(1, 490)
         self.rect.y = randint(1, 360)
-        self.speed = 1 + random() * 2
+        self.speed = speed + random() * 2
 
     def update(self, *args):
         self.rect.x -= self.speed
@@ -54,9 +60,9 @@ class Shot(sprite.Sprite):
     def __init__(self, *groups):
         super().__init__(*groups)
 
-        self.image = image.load(r"C:\Users\lucas.paula_kovi\VSCodeProjects\MyOwnProjects\asteroids_destroy\game_assets\data\Shot.png")
-        self.image = transform.scale(self.image, [50, 50])
-        self.rect = Rect([50, 50, 50, 50])
+        self.image = image.load(r"C:\Users\lucas.paula_kovi\VSCodeProjects\MyOwnProjects\asteroids_destroy\game_assets\data\Shot_1.png")
+        self.image = transform.scale(self.image, [24, 9])
+        self.rect = Rect([24, 9, 24, 9])
         self.speed = 4
 
     def update(self, *args):
